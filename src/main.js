b4346394e8ef1527853ef1b65147c26832d83545
Max Ploner <m.ploner@campus.tu-berlin.de>
2021-05-28 13:06:04 +0200

    Worked on abstracting graphical elements into components

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Looking_for_theta from './components/looking_for_theta.html';
-import MetaGradient from './components/metaGradient.html'
-
-console.log(document.querySelector('#looking_for_theta'))
-
 const Looking_for_theta_Component = new Looking_for_theta({ // eslint-disable-line no-unused-vars
   target: document.querySelector('#looking_for_theta')
 });
 
+
+/*
+import MetaGradient from './components/metaGradient.html'
 const MetaGradient_Component = new MetaGradient({ // eslint-disable-line no-unused-vars
   target: document.querySelector('#metaGradient')
+})*/
+
+import ContourExample from './components/contourExample.html'
+const ContourExample_Component = new ContourExample({ // eslint-disable-line no-unused-vars
+  target: document.querySelector('#contourExample')
 })
