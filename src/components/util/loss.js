95502fe6fdac0ee12870b70b616ccd1834c89ad7
Max Ploner <m.ploner@campus.tu-berlin.de>
2021-06-01 16:49:21 +0200

    Made element more reactive

--- src/components/util/loss.js
+++ src/components/util/loss.js
@@ -3,7 +3,7 @@ import * as tf from '@tensorflow/tfjs'
 
 
 // Some radial basis function
-const RBF = (phi, center, factor) => tf.tidy(() => {
+const RBF = (phi, center, factor) => {
   var additional_phi_dims = phi.shape.slice(0, -1);
   var additional_center_dims = center.shape.slice(0, -1);
 
@@ -16,17 +16,16 @@ const RBF = (phi, center, factor) => tf.tidy(() => {
       -1  // sum over last axis
     ).mul(-10)
   ).mul(factor);
-
-});
+};
 
 // Loss for each task (sum of some RBFs)
-export const RBFLoss = (phi, center, factor) => tf.tidy(() => {
+export const RBFLoss = (phi, center, factor) => {
   var rbfs = RBF(phi, center, factor);
   return tf.sum(rbfs, -1);
-});
+};
 
 
-export const himmelblau = phi => tf.tidy(() => {
+export const himmelblau = phi => {
   var [x, y] = phi.split(2, -1);
   x = x.mul(12).sub(6);
   y = y.mul(12).sub(6);
@@ -34,18 +33,18 @@ export const himmelblau = phi => tf.tidy(() => {
   return (x.square().add(y).sub(11)).square().add(
     (x.add(y.square()).sub(7)).square()
   )
-})
+}
 
 
-export const beale = phi => tf.tidy(() => {
+export const beale = phi => {
   var [x, y] = phi.split(2, -1);
   var x = x.mul(3.5).sub(1.5);
   var y = y.mul(3.5).sub(0.5);
   return tf.scalar(1).sub(x).square().add(y.sub(x.square()).square().mul(100))
-});
+};
 
 
-export const rosenbrock = phi => tf.tidy(() => {
+export const rosenbrock = phi => {
   // Beale Function
   var [x, y] = phi.split(2, -1);
   var x = x.mul(9).sub(4.5);
@@ -63,4 +62,4 @@ export const rosenbrock = phi => tf.tidy(() => {
 
 
   return result;
-});
+};
