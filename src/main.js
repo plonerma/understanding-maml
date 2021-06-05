d51845bad25fbdf12c8ebf1cd7eadb4c07c9701a
pupuis <luismueller2309@gmail.com>
2021-06-05 17:52:49 +0200

    new fit sine component

--- src/main.js
+++ src/main.js
@@ -14,3 +14,8 @@ import ContourExample from './components/contourExample.html'
 const ContourExample_Component = new ContourExample({
   target: document.querySelector('#contourExample')
 })
+
+import FitSine from './components/fitSine.html'
+const FitSine_Component = new FitSine({
+  target: document.querySelector('#fitSine')
+})
